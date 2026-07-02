548ca192a1a2f9a374955cf5e047ca2b3980c619
Nekena Andriniaina <andriniainanekena@gmail.com>
2026-07-03 01:00:34 +0300

    feat: wire main entry point to game screen

--- core/src/main/java/com/firstime/libgdx/Main.java
+++ core/src/main/java/com/firstime/libgdx/Main.java
@@ -1,11 +1,12 @@
 package com.firstime.libgdx;
 
 import com.badlogic.gdx.Game;
+import com.firstime.libgdx.screen.GameScreen;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+
     @Override
     public void create() {
-        setScreen(new FirstScreen());
+        setScreen(new GameScreen());
     }
 }
